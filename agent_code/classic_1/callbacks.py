a1e0a62166d4b0943115634cdfd69a7e953a1513
Abhinand P <abhinand.po@gmail.com>
2023-09-17 21:44:58 +0200

    Added new feature: Agent presence

--- agent_code/classic_1/callbacks.py
+++ agent_code/classic_1/callbacks.py
@@ -12,7 +12,7 @@ def setup(self):
     if self.train or not os.path.isfile(os.path.join(q_table_folder, "q_table.npy")):
         self.logger.info("Q-Learning algorithm.")
         self.timestamp = datetime.now().strftime("%dT%H:%M:%S")
-        self.number_of_states = 4
+        self.number_of_states = 6
         self.Q_table = np.zeros(shape=(self.number_of_states, len(ACTIONS)))
         self.exploration_rate = 0.5
         self.exploration_rate_initial = 0.5
@@ -43,11 +43,11 @@ def act(self, game_state: dict) -> str:
         return action
 
 
-def _get_neighboring_tiles(own_coord, n) -> List[Tuple[int]]:
+def _get_neighboring_tiles(own_coord, radius) -> List[Tuple[int]]:
     x, y = own_coord
     # Finding neighbouring tiles
     neighboring_coordinates = []
-    for i in range(1, n + 1):
+    for i in range(1, radius + 1):
         neighboring_coordinates.extend([
             (x, y + i),  # down in the matrix
             (x, y - i),  # up in the matrix
@@ -57,6 +57,32 @@ def _get_neighboring_tiles(own_coord, n) -> List[Tuple[int]]:
     return neighboring_coordinates
 
 
+# Feature 1: Count the number of walls in the immediate surrounding tiles within a given radius.
+def count_walls(own_position, game_state, radius):
+    return sum(
+        1 for coord in _get_neighboring_tiles(own_position, radius)
+        if 0 <= coord[0] < game_state["field"].shape[0] and 0 <= coord[1] < game_state["field"].shape[1]
+        and game_state["field"][coord] == -1
+    )
+
+
+# Feature 2: Check for bomb presence in the immediate surrounding tiles within a given radius.
+def check_bomb_presence(own_position, game_state, radius):
+    return any(
+        bomb[0] in _get_neighboring_tiles(own_position, radius)
+        and bomb[1] != 0
+        for bomb in game_state["bombs"]
+    )
+
+
+# Feature 3: Check for agent presence in the immediate surrounding tiles within a given radius.
+def check_agent_presence(own_position, game_state, radius):
+    return any(
+        agent[3] in _get_neighboring_tiles(own_position, radius)
+        for agent in game_state["others"]
+    )
+
+
 def state_to_features(game_state, history) -> np.array:
     if game_state is None:
         print("First game state is None")
@@ -64,20 +90,13 @@ def state_to_features(game_state, history) -> np.array:
 
     own_position = game_state["self"][-1]
 
-    # Count the number of walls in neighboring tiles
-    wall_counter = sum(
-        1 for coord in _get_neighboring_tiles(own_position, 3)
-        if 0 <= coord[0] < game_state["field"].shape[0] and 0 <= coord[1] < game_state["field"].shape[1]
-        and game_state["field"][coord] == -1
-    )
+    # Calculate features
+    wall_counter = count_walls(own_position, game_state, 3)
+    bomb_present = check_bomb_presence(own_position, game_state, 3)
+    agent_present = check_agent_presence(own_position, game_state, 3)
 
-    # Check if there are bombs within a radius of 3
-    bomb_present = any(
-        bomb[0] in _get_neighboring_tiles(own_position, 3)
-        and bomb[1] != 0
-        for bomb in game_state["bombs"]
-    )
-    features = np.array([int(wall_counter > 2), int(bomb_present)])
-    feature_id = 2 * features[0] + features[1]
+    # Calculate feature_id based on features
+    features = np.array([int(wall_counter > 2), int(bomb_present), int(agent_present)])
+    feature_id = 2 * features[0] + features[1] + 2 * features[2]
 
     return feature_id
