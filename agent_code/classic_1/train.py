ede02a5df658963c898a24e7a66bd8d4f7aafb69
Abhinand P <abhinand.po@gmail.com>
2023-09-21 20:33:18 +0200

    Custom event: BOMB_DISTANCE_NEAR and BOMB_DISTANCE_FAR

--- agent_code/classic_1/train.py
+++ agent_code/classic_1/train.py
@@ -38,9 +38,9 @@ def setup_training(self):
     # Initial exploration rate for training
     self.exploration_rate = self.exploration_rate_initial
     # Alpha = Learning Rate.
-    self.learning_rate = 0.1
+    self.learning_rate = 0.5
     # Gamma = Discount Rate.
-    self.discount_rate = 0.99
+    self.discount_rate = 0.2
     # episode number
     self.episodes = 0.0
     # Gathered return of rewards per episode
@@ -53,44 +53,48 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     self.logger.debug(
         f'Classic_1 Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
 
-    # Skip the first game state
-    if old_game_state is None:
-        print("First game state is None")
-        return
+    self.history.append(new_game_state["self"][-1])
+    old_state = self.old_state
+    self.new_state = state_to_features(self, new_game_state)
+    new_state = self.new_state
+    old_feature_dict = self.valid_list[old_state]
 
-    self.transitions.append(Transition(state_to_features(self,old_game_state, self.history), self_action,
-                                       state_to_features(self,new_game_state, self.history),
-                                       reward_from_events(self, events), ))
+    if old_feature_dict["Direction_bomb"] != "SAFE":
+        if self_action == old_feature_dict["Direction_bomb"]:
+            events.append(BOMB_DISTANCE_FAR)
+        else:
+            events.append(BOMB_DISTANCE_NEAR)
 
-    state, action, next_state, reward = (
-        self.transitions[-1][0], self.transitions[-1][1], self.transitions[-1][2], self.transitions[-1][3],)
+    reward = reward_from_events(self, events)
+    self.transitions.append(Transition(old_state, self_action, new_state, reward))
 
-    action_idx = ACTION_INDEX[action]
-    self.logger.debug(f"Action: {action}, Action Index: {action_idx}")
-    self.logger.debug(f"Old Q-value for state {state}: {self.Q_table[state, action_idx]}")
+    action_idx = ACTION_INDEX[self_action]
+    self.logger.debug(f"game_events_occurred: game_events_occurred: Action: {self_action}, Action Index: {action_idx}")
+    self.logger.debug(f"game_events_occurred: Old Q-value for state {old_state}: {self.Q_table[old_state]}")
 
     self.episode_gathered_rewards += reward
 
-    new_q_value = self.Q_table[state, action_idx] + self.learning_rate * (
-            reward + self.discount_rate * np.max(self.Q_table[next_state]) - self.Q_table[state, action_idx])
+    new_q_value = self.Q_table[old_state, action_idx] + self.learning_rate * (
+            reward + self.discount_rate * np.max(self.Q_table[new_state]) - self.Q_table[old_state, action_idx])
 
-    self.Q_table[state, action_idx] = new_q_value
+    self.Q_table[old_state, action_idx] = new_q_value
 
     # self.logger.debug(f"Updated Q-value for state {state} and action {action}: {new_q_value}")
-
     # self.logger.debug(f"Classic_1 Updated Q-table: {self.Q_table}")
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
-    self.transitions.append(Transition(state_to_features(self,last_game_state, self.history), last_action, None,
-                                       reward_from_events(self, events), ))
+    self.transitions.append(Transition(state_to_features(self, last_game_state), last_action, None,
+                                       reward_from_events(self, events)))
 
     self.episode_gathered_rewards += self.transitions[-1][3]
 
-    self.logger.debug(f"Total rewards in episode {self.episodes}: {self.episode_gathered_rewards}")
+    self.logger.debug(f"end_of_round: Total rewards in episode {self.episodes}: {self.episode_gathered_rewards}")
 
-    # Reset the gathered rewards to 0 after each episode
-    self.episode_gathered_rewards = 0
+    # Performing exploration rate decay
+    self.exploration_rate = self.exploration_rate_end + (
+            self.exploration_rate_initial - self.exploration_rate_end) * np.exp(
+        -self.exploration_decay_rate * self.episodes)
 
     # TODO: Should I update the same Q-table instead of new one everytime?
     q_table_folder = "Q_tables/"
@@ -98,12 +102,9 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
         q_table_file = os.path.join(q_table_folder, f"Q_table-{self.timestamp}")
         np.save(q_table_file, self.Q_table)
 
+    self.episode_gathered_rewards = 0
     self.episodes += 1
 
-    # Performing exploration rate decay
-    self.exploration_rate = self.exploration_rate_end + (
-            self.exploration_rate_initial - self.exploration_rate_end) * np.exp(
-        -self.exploration_decay_rate * self.episodes)
     # self.logger.debug(f"Exploration_rate{self.episodes}: {self.exploration_rate}")
 
 
@@ -124,6 +125,8 @@ def reward_from_events(self, events: List[str]) -> int:
         # e.MOVED_UP: 3,
         # e.MOVED_DOWN: 3,
         e.INVALID_ACTION: -1,
+        BOMB_DISTANCE_NEAR: -10,
+        BOMB_DISTANCE_FAR: 20,
         PLACEHOLDER_EVENT: -1
 
     }
@@ -133,4 +136,3 @@ def reward_from_events(self, events: List[str]) -> int:
             reward_sum += game_rewards[event]
     # self.logger.debug(f"Awarded {reward_sum} for events {', '.join(events)}")
     return reward_sum
-
