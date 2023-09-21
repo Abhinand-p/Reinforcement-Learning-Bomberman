bd4509834d303918b80f88fd1e7618e05876516e
Abhinand P <abhinand.po@gmail.com>
2023-09-21 18:00:27 +0200

    Feature: Shortest Path to Coin/Crate - 2

--- agent_code/classic_1/callbacks.py
+++ agent_code/classic_1/callbacks.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import math
 import networkx as nx
+import itertools
 
 from datetime import datetime
 from typing import Tuple, List
@@ -10,23 +11,23 @@ from igraph import Graph
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 ACTIONS_IDEAS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
-
-# TODO: Check if this values will change. If yes then I can read from settings.py
 n_rows = 17
 n_cols = 17
 
 
 def setup(self):
     q_table_folder = "Q_tables/"
+    self.valid_list = Valid_States()
     self.history = [0, deque(maxlen=5)]  # Currently holding (number of coins collected, tiles visited)
     self.new_state = None
+    self.old_state = None
     self.old_distance = 0
     self.new_distance = 0
 
     if self.train:
         self.logger.info("Q-Learning algorithm.")
         self.timestamp = datetime.now().strftime("%dT%H:%M:%S")
-        self.number_of_states = 10  # TODO: I think this should be dynamic and not a static number.
+        self.number_of_states = len(self.valid_list)
         self.Q_table = np.zeros(shape=(self.number_of_states, len(ACTIONS)))  # number_of_states * 6
         self.exploration_rate_initial = 1.0
         self.exploration_rate_end = 0.05
@@ -34,8 +35,7 @@ def setup(self):
 
     else:
         self.logger.info("Loading from the latest Q_table")
-        q_table_directory_path = "Q_tables"
-        self.Q_table = load_latest_q_table(self, q_table_directory_path)
+        self.Q_table = load_latest_q_table(self, q_table_folder)
 
 
 def set_decay_rate(self) -> float:
@@ -47,7 +47,14 @@ def set_decay_rate(self) -> float:
 
 
 def act(self, game_state: dict) -> str:
-    state = state_to_features(self, game_state, self.history)
+    # Testing this logic
+    if self.new_state is None:
+        self.old_state = state_to_features(self, game_state)
+    else:
+        self.old_state = self.new_state
+
+    state = self.old_state
+    self.logger.info(f"act: State: {state}")
 
     if self.train and np.random.random() < self.exploration_rate:
         # TODO: Check if during exploring random choice is the best option because we do not want self explosions.
@@ -57,12 +64,29 @@ def act(self, game_state: dict) -> str:
 
     if not np.any(self.Q_table[state]):
         action = np.random.choice(ACTIONS)
-        self.logger.info(f"Q-Table has no zeros, so random action chosen:{action}")
+        self.logger.info(f"Q-Table has all zeros, so random action chosen: {action}")
     else:
         action = ACTIONS[np.argmax(self.Q_table[state])]
         self.logger.info(f"Exploiting: {action}")
-
     return action
+
+
+def Valid_States() -> np.array:
+    feature_list = []
+    valid_states = list(itertools.product(('UP', 'RIGHT', 'DOWN', 'LEFT'), ('UP', 'RIGHT', 'DOWN', 'LEFT', 'SAFE'),
+                                          ('MOVE', 'BLOCK'), ('MOVE', 'BLOCK'), ('MOVE', 'BLOCK'), ('MOVE', 'BLOCK')))
+
+    for states in valid_states:
+        features = {
+            "Direction_coin/crate": states[0],
+            "Direction_bomb": states[1],
+            "Up": states[2],
+            "Right": states[3],
+            "Down": states[4],
+            "Left": states[5],
+        }
+        feature_list.append(features)
+    return feature_list
 
 
 def get_neighboring_tiles(own_coord, radius) -> List[Tuple[int]]:
@@ -163,13 +187,13 @@ def select_best_action(self, current_coord, next_coord):
     y_diff = next_coord[1] - current_coord[1]
 
     # ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT']
-    if x_diff == 1:
+    if np.any(x_diff == 1):
         return "RIGHT"  # RIGHT
-    elif x_diff == -1:
+    elif np.any(x_diff == -1):
         return "LEFT"  # LEFT
-    elif y_diff == 1:
+    elif np.any(y_diff == 1):
         return "UP"  # UP
-    elif y_diff == -1:
+    elif np.any(y_diff == -1):
         return "DOWN"  # DOWN
 
 
@@ -257,35 +281,48 @@ def calculate_going_to_new_tiles(history):
 
 
 def shortest_path_to_coin_or_crate(agent, game_state):
+    graph = calculate_adjacency_matrix(agent, game_state)
     graph_with_crates = calculate_adjacency_matrix(agent, game_state, consider_crates=False)
 
     # current coordinate Classic_1 agent
     current_position = game_state["self"][-1]
-    # agent.logger.info(f"shortest_path_to_coin: current_position: {current_position}")
+    # agent.logger.info(f"shortest_path_to_coin_or_crate: current_position: {current_position}")
 
     # Extract explosion area positions.
     explosion_area = [(index[0], index[1]) for index, field in np.ndenumerate(game_state["explosion_map"]) if
                       field != 0]
-    # agent.logger.info(f"shortest_path_to_coin: explosion_area: {explosion_area}")
+    # agent.logger.info(f"shortest_path_to_coin_or_crate: explosion_area: {explosion_area}")
 
     # Crates present that are not yet exploded.
     crates = [(index[0], index[1]) for index, field in np.ndenumerate(game_state["field"]) if field == 1]
-    # agent.logger.info(f"shortest_path_to_coin: crates: {crates}")
+    # agent.logger.info(f"shortest_path_to_coin_or_crate: crates: {crates}")
 
     # Good Coins are those that are not in the explosion_area
     good_coins = [coin for coin in game_state["coins"] if coin not in explosion_area]
-    # agent.logger.info(f"shortest_path_to_coin: good_coins: {good_coins}")
+    # agent.logger.info(f"shortest_path_to_coin_or_crate: good_coins: {good_coins}")
 
-    #  If only there are crates but no good_coins
-    if not any(good_coins) and any(crates):
+    # If no coins and crates -> Random ?
+    if not any(good_coins) and not any(crates):
+        random_choice = np.random.choice(ACTIONS_IDEAS)
+        agent.logger.info(f"shortest_path_to_coin_or_crate: No Coins and Crates {random_choice}")
+        return random_choice
+
+    # If only there are crates but no good_coins
+    elif not any(good_coins):
         next_crate_position = (None, np.inf)
 
         for crate_coord in crates:
-            current_path, current_path_length = find_shortest_path_coordinates(graph_with_crates, current_position,
-                                                                               crate_coord)
+            try:
+                current_path, current_path_length = find_shortest_path_coordinates(graph_with_crates, current_position,
+                                                                                   crate_coord)
+
+            except nx.exception.NetworkXNoPath:
+                agent.logger.info("shortest_path_to_coin_or_crate: Crate may be exploded")
+                continue
+
             if current_path_length == 1:
                 # TODO: Should I modify the code to add an action to bomb here for this case?
-                agent.logger.info(f"shortest_path_to_coin: I am next to a crate")
+                agent.logger.info(f"shortest_path_to_coin_or_crate: Agent next to a crate")
                 return select_best_action(agent, current_position, current_path[0])
 
             elif current_path_length < next_crate_position[1]:
@@ -294,58 +331,125 @@ def shortest_path_to_coin_or_crate(agent, game_state):
         # If there are no crates or coins still then return a random action -> highly unlikely I guess
         if next_crate_position == (None, np.inf):
             random_choice = np.random.choice(ACTIONS_IDEAS)
-            agent.logger.info(f"shortest_path_to_coin: no crate or a good coin still: {random_choice}")
+            agent.logger.info(f"shortest_path_to_coin_or_crate: no crate or a good coin still: {random_choice}")
             return random_choice
 
         return select_best_action(agent, current_position, next_crate_position[0][0])
 
-    # If there are no crates or good coins -> random action
-    elif not any(good_coins) and not any(crates):
-        random_choice = np.random.choice(ACTIONS_IDEAS)
-        agent.logger.info(f"shortest_path_to_coin: no crate or a good coin: {random_choice}")
-        return random_choice
-
+    # If there are only good coins
     else:
-        random_choice = np.random.choice(ACTIONS_IDEAS)
-        agent.logger.info(f"shortest_path_to_coin: Last else: {random_choice}")
-        return random_choice
+        coin_path = []
+
+        # Finding the shortest path for all good coins
+        for coin_coord in good_coins:
+            try:
+                current_path, current_path_length = find_shortest_path_coordinates(graph, current_position, coin_coord)
+                current_reachable = True
+            except nx.exception.NetworkXNoPath:
+                try:
+                    current_path, current_path_length = find_shortest_path_coordinates(graph_with_crates,
+                                                                                       current_position, coin_coord)
+                    current_reachable = False
+                except nx.exception.NetworkXNoPath:
+                    agent.logger.info("shortest_path_to_coin_or_crate: Corner case 1")
+                    continue
+
+            # If there are no other agents alive
+            if not any(game_state["others"]):
+                coin_path.append(((current_path, current_path_length, current_reachable), (None, np.inf)))
+                continue
+
+            for other_agent in game_state["others"]:
+                best_other_agent = (None, np.inf)
+                other_agent_coord = other_agent[3]
+                try:
+                    current_path_other_agent, current_path_length_other_agent = find_shortest_path_coordinates(graph,
+                                                                                                               other_agent_coord,
+                                                                                                               coin_coord)
+                    current_other_agent_reachable = True
+
+                except nx.exception.NetworkXNoPath:
+                    try:
+                        current_path_other_agent, current_path_length_other_agent = find_shortest_path_coordinates(
+                            graph_with_crates, other_agent_coord, coin_coord)
+                        current_other_agent_reachable = False
+
+                    except nx.exception.NetworkXNoPath:
+                        agent.logger.info("shortest_path_to_coin_or_crate: Corner case 2")
+                        continue
+
+                if not current_other_agent_reachable:
+                    current_path_length_other_agent += 7
+
+                if current_path_length_other_agent < best_other_agent[1]:
+                    best_other_agent = (
+                        current_path_other_agent, current_path_length_other_agent, current_other_agent_reachable)
+            coin_path.append(((current_path, current_path_length, current_reachable), best_other_agent))
+
+        # If no coins are still not reachable
+        if not any(coin_path):
+            random_choice = np.random.choice(ACTIONS_IDEAS)
+            agent.logger.info(f"shortest_path_to_coin_or_crate: Corner case 3: {random_choice}")
+            return random_choice
+
+        # Sorting the obtained coin paths
+        coin_path.sort(key=lambda x: x[0][1])
+        coin_path_reachable = [path_to_coin[0][2] for path_to_coin in coin_path]
+
+        # If no paths are reachable to coins
+        if not any(coin_path_reachable):
+            return select_best_action(agent, current_position, coin_path[0][0])
+
+        # If there is only one coin path reachable
+        elif coin_path_reachable.count(True) == 1:
+            agent.logger.info(f"shortest_path_to_coin_or_crate: Exactly one coin path is reachable")
+            index_of_reachable_path = coin_path_reachable.index(True)
+            return select_best_action(agent, current_position, coin_path[index_of_reachable_path][0][0])
+
+        for path_to_coin in coin_path:
+            if path_to_coin[0][2] is True and path_to_coin[0][1] <= path_to_coin[1][1] and path_to_coin[0][1] != 0:
+                return select_best_action(agent, current_position, path_to_coin[0][0])
+        try:
+            return select_best_action(agent, current_position, coin_path[0][0][0])
+
+        except IndexError:
+            random_choice = np.random.choice(ACTIONS_IDEAS)
+            agent.logger.info(f"shortest_path_to_coin_or_crate: Corner case 4: {random_choice}")
+            return random_choice
 
 
-# TODO: I should make a feature dictionary and return the action list. Then I will have a better contol to check the
-#  training process
-def state_to_features(self, game_state, history) -> np.array:
+def state_to_features(self, game_state) -> np.array:
     if game_state is None:
-        print("First game state is None")
-        return np.zeros(5)
+        self.logger.info(f"state_to_features: First Game")
+    features_dict = {}
 
-    current_position = game_state["self"][-1]
-    enemy_positions = [enemy[-1] for enemy in game_state["others"]]
-    adj_graph = calculate_adjacency_matrix(self, game_state, True)
+    # Feature 1:
+    coin_direction = shortest_path_to_coin_or_crate(self, game_state)
+    if coin_direction in ["DOWN", "UP", "RIGHT", "LEFT"]:
+        features_dict["Direction_coin/crate"] = coin_direction
+        self.logger.info(f"state_to_features: Feature 1:{coin_direction}")
+    else:
+        self.logger.info(f"state_to_features: Feature 1: Invalid direction")
 
-    features = np.zeros(10, dtype=np.int8)
-
+    # features = np.zeros(10, dtype=np.int8)
     # Calculate features
     # wall_counter = count_walls(current_position, game_state, 3)
     # bomb_present = check_bomb_presence(current_position, game_state, 3)
     # agent_present = check_agent_presence(current_position, game_state, 3)
-    death_tile = calculate_death_tile(game_state, current_position)
-    features[0] = death_tile
-
-    blockage_features = compute_blockage(game_state, current_position)
-    features[1:5] = blockage_features
-
-    visited_ratio = calculate_going_to_new_tiles(self.history)
-    features[5] = visited_ratio
-
-    direction = shortest_path_to_coin_or_crate(self, game_state)
-
+    # death_tile = calculate_death_tile(game_state, current_position)
+    # features[0] = death_tile
+    # blockage_features = compute_blockage(game_state, current_position)
+    # features[1:5] = blockage_features
+    # visited_ratio = calculate_going_to_new_tiles(self.history)
+    # features[5] = visited_ratio
     # Calculate feature_id based on features
     # features = np.array([int(wall_counter > 2), int(bomb_present), int(agent_present), int(death_tile)])
     # feature_id = 2 * features[0] + features[1] + 2 * features[2] + features[3]
 
-    self.logger.info(f"Feature vector: {features}")
-    self.logger.info(f"Adjacency Graph: {adj_graph}")
-    return features
+    self.logger.info(f"Feature Dictionary: {features_dict}")
+    for i, state in enumerate(self.valid_list):
+        if state == features_dict:
+            return i
 
 
 def load_latest_q_table(self, q_table_directory):
